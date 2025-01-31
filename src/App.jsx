3e7625e44873a04be58bb921bb4faf8361c41162
Raquel Fernandez <rmfernandezbermudez@gmail.com>
2025-01-31 14:27:07 +0100

    Implementando lógica Parte1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import "./App.css";
+import { useState, useRef } from "react";
+
+const number = Math.trunc(Math.random() * 20) + 1;
 
 function App() {
+  const [score, setScore] = useState(20);
+  const [highscore, setHighscore] = useState(0);
+  const inputRef = useRef(null);
+
+  const handleCheck = () => {
+    //Comprobar si el valor introducido es igual al numero aleatorio
+    console.log(inputRef.current.value);
+    const guess = Number(document.querySelector(".guess").value);
+    setScore(score - 1);
+  };
+
   return (
     <>
       {" "}
@@ -12,16 +26,18 @@ function App() {
       </header>
       <main>
         <section className="left">
-          <input type="number" className="guess" />
-          <button className="btn check">Check!</button>
+          <input type="number" className="guess" ref={inputRef} />
+          <button className="btn check" onClick={handleCheck}>
+            Check!
+          </button>
         </section>
         <section className="right">
           <p className="message">Start guessing...</p>
           <p className="label-score">
-            💯 Score: <span className="score">20</span>
+            💯 Score: <span className="score">{score}</span>
           </p>
           <p className="label-highscore">
-            🥇 Highscore: <span className="highscore">0</span>
+            🥇 Highscore: <span className="highscore">{highscore}</span>
           </p>
         </section>
       </main>
